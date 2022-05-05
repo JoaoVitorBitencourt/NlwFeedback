1d5a517202767af4b33ba43f4bf348f529d9dae4
joao.bitencourt <joao.bitencourt@useall.com.br>
2022-05-05 14:48:09 -0300

    criado server e adicionado funcionalidade de email

---
+++ src/server.ts
@@ -0,0 +1,46 @@
+import express from "express";
+import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
+
+const app = express();
+
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "3757ca5e3bf612",
+      pass: "87bc9c6cb7558a"
+    }
+  });
+
+app.post("/feedbacks", async (req, res) => {
+    const { type, comment, screenshot } = req.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot
+        }
+    });
+
+    await transport.sendMail({
+        from: 'Equipe Feedget <teste@feedget.com>',
+        to: 'João Vitor <joaov.vbsb@gmail.com>',
+        subject: 'Novo feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`,
+            `</div>`
+        ].join('\n')
+    });
+
+    return res.status(201).json({ data: feedback });
+})
+
+app.listen(3333, () => {
+    console.log("HTTP server running!");
+})
